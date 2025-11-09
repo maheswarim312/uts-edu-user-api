bf775cdad25acc3e9a6c10f9c131e8f98bad39d1
mahesmahfud <mahesmahfud@gmail.com>
2025-11-10 02:37:09 +0700

    fix: Manually register MailtrapApiTransport in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport\Dsn;
+use Symfony\Component\Mailer\Bridge\Mailtrap\Transport\MailtrapApiTransport;
+use Psr\Log\LoggerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Mail::extend('mailtrap+api', function () {
+            $config = $this->app['config']->get('mail.mailers.mailtrap');
+
+            if (empty($config['token']) || empty($config['inbox_id'])) {
+                 throw new \Exception('Mailtrap token and inbox_id are required.');
+            }
+
+            return new MailtrapApiTransport(
+                $config['token'],
+                $config['inbox_id'],
+                $this->app->make('http_client'),
+                $this->app->make(LoggerInterface::class)
+            );
+        });
     }
 }
