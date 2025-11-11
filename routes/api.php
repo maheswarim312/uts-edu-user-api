8e9d3362421f50950101ae683e087ed2deff6cda
mahesmahfud <mahesmahfud@gmail.com>
2025-11-11 23:21:13 +0700

    profile semua akun bs

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile/me', [ProfileController::class, 'show']);
     Route::put('/profile/me', [ProfileController::class, 'update']);
 
+    Route::get('/users', [UserController::class, 'index']);
+
     Route::middleware('can:is-admin')->group(function () {
-        Route::get('/users', [UserController::class, 'index']);
         Route::post('/users', [UserController::class, 'store']);
         Route::put('/users/{id}', [UserController::class, 'update']);
         Route::delete('/users/{id}', [UserController::class, 'destroy']);
